efb0f74e2c174c3edd84ec4c4445af4ed93842b9
nicocavallu <nicocavalluzzi@gmail.com>
2026-06-29 16:05:47 -0400

    Added sine LUT module with python script to generate wave

---
+++ scripts/gen_sin_table.py
@@ -0,0 +1,27 @@
+# gen_sine_table.py 
+''' 
+Pyhon script to generate a sine wave for the LUT table 
+'''
+
+import numpy as np
+
+n = 1024 # (2^10)
+
+amp = 131071 # (2^17 - 1)
+
+step = 2.0 * np.pi / n  
+
+with open("sine_table.mem", "w") as f:
+    for i in range(n):
+        angle = i * step 
+        val = 0
+        raw_val = int(round(amp * np.sin(angle)))
+
+        if raw_val < 0:
+            val = (1 << 18 ) + raw_val
+        else:
+            val = raw_val
+
+    f.write(f"{val:05X}\n")
+
+print("Succesfully generated sine_table.mem with 1024 18-bit values")
---
+++ src/sine_lut.v
@@ -0,0 +1,24 @@
+/* sine_lut.v
+* This module is a 10-input LUT that outputs a sine wave
+* the input is based on the truncated output of the phase accumulator
+* */
+
+module sine_lut(
+    input clk, enable,
+    input [9:0] phase_truncated,
+    output reg [17:0] sine_out);
+
+    reg [17:0] rom [1024];
+
+    initial begin
+        $readmemh("../scripts/sine_table.mem", rom);
+    end
+
+    always @(posedge clk) begin
+        if (enable) begin
+            sine_out <= rom[phase_truncated];
+        end else begin
+            sine_out <= sine_out;
+        end
+    end
+endmodule
